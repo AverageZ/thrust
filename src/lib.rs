5c7dc86f66c40579e351667b63be5d32c14c124d
AverageZ <alexzajac@live.com>
2017-03-11 15:44:04 -0500

    [vga|patch] Printing to screen from rust code

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,32 @@
 #![feature(lang_items)]
+#![feature(const_fn, unique)]
 #![no_std]
 
-#[lang = "eh_personality"]
-extern fn eh_personality() {
-}
+extern crate rlibc;
+extern crate volatile;
+extern crate spin;
 
+#[macro_use]
+mod vga_buffer;
+
+#[cfg(not(test))]
+#[lang = "eh_personality"]
+extern "C" fn eh_personality() {}
+
+#[cfg(not(test))]
 #[lang = "panic_fmt"]
-extern fn rust_begin_panic() -> ! {
-  loop {}
-}
+#[no_mangle]
+pub extern "C" fn panic_fmt() -> ! {loop{}}
+
+#[allow(non_snake_case)]
+#[no_mangle]
+pub extern "C" fn _Unwind_Resume() -> ! {loop {}}
 
 #[no_mangle]
-pub extern fn kmain() -> ! {
-  unsafe {
-    let vga = 0xb8000 as *mut u64;
-    *vga = 0x2f592f412f4b2f4f;
-  };
+pub extern "C" fn kmain() {
+  // ATTENTION: we have a very small stack and no guard page
+  vga_buffer::clear_screen();
+  println!("Hello World{}", "!");
 
-  loop {}
+  loop{}
 }
